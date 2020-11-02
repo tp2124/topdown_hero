71f8d9aacbe042945fc9e5edf0db7aca0d3a0e62
tp2124 <tp2124@gmail.com>
2020-11-01 19:33:06 -0600

    Adding Interactable object that changes material on enter and exit of it's sphere collision.

---
+++ Source/topdown_hero/InteractableObject.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractableObject.h"
+
+#include "topdown_hero/topdown_heroCharacter.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AInteractableObject::AInteractableObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AInteractableObject::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AInteractableObject::PlayEffects() const 
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
+}
+
+void AInteractableObject::UpdateMaterial(bool isInteractable)
+{
+	MeshComp->SetMaterial(0, isInteractable ? InteractableMaterial : OutOfRangeMaterial);
+}
+
+// Called every frame
+void AInteractableObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AInteractableObject::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	PlayEffects();
+
+	//Atopdown_heroCharacter* character = Cast<Atopdown_heroCharacter>(OtherActor);
+	UpdateMaterial(true);
+}
+
+void AInteractableObject::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+
+	//Atopdown_heroCharacter* character = Cast<Atopdown_heroCharacter>(OtherActor);
+	UpdateMaterial(false);
+}
+
---
+++ Source/topdown_hero/InteractableObject.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractableObject.generated.h"
+
+class USphereComponent;
+class UParticleSystem;
+class Atopdown_heroCharacter;
+
+UCLASS()
+class TOPDOWN_HERO_API AInteractableObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractableObject();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent* SphereComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Materials")
+	UMaterial* OutOfRangeMaterial;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Materials")
+	UMaterial* InteractableMaterial;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	UParticleSystem* PickupFX;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void PlayEffects() const;
+	void UpdateMaterial(bool isInteractable);
+	bool IsPlayerInRange(const Atopdown_heroCharacter* character) const;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor);
+
+	virtual void NotifyActorEndOverlap(AActor* OtherActor);
+
+};
